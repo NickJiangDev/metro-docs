8f9e3d6c96f1157592f6b052794dcd2d66d16733
姜杉 <jiangshan@metrotech.com>
2023-04-10 14:36:15 +0800

    feat: 🎸 update doc

---
+++ docs__basic__global-dark__index.md.28206499.async.js
@@ -0,0 +1,10 @@
+"use strict";(self.webpackChunk_metro_docs=self.webpackChunk_metro_docs||[]).push([[511],{94908:function(d,s,_){_.r(s);var i=_(72086),l=_(86530),r=_(12),m=_(4396),u=_(40384),a=_(63510),n=_(27120),E=_(41398),e=_(46228);function t(){var o=(0,n.eL)(),D=o.texts;return(0,e.jsx)(n.dY,{children:(0,e.jsxs)(e.Fragment,{children:[(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsxs)("h1",{id:"\u6697\u8272-global-token",children:[(0,e.jsx)("a",{"aria-hidden":"true",tabIndex:"-1",href:"#\u6697\u8272-global-token",children:(0,e.jsx)("span",{className:"icon icon-link"})}),"\u6697\u8272 Global Token"]}),(0,e.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+    h1 {
+        color: #EAEBEC !important;
+    }
+    h2 {
+        color: #EAEBEC !important;
+    }
+    .dumi-default-content { background-color: #2D303E !important; color: #EAEBEC !important; }
+
+`}})]}),(0,e.jsx)(n.Dl,{demo:{id:"docs-basic-global-dark-demo-colors",inline:!0},previewerProps:{}})]})})}s.default=t}}]);
